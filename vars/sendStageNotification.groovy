5e5f558e7e4cd8c4566ca1add18075c1ef265ce0
basmaoueslati <47010064+basmaoueslati@users.noreply.github.com>
2025-06-03 22:29:52 +0300

    Update sendStageNotification.groovy

--- vars/sendStageNotification.groovy
+++ vars/sendStageNotification.groovy
@@ -1,9 +1,15 @@
 def call(String recipient) {
+    // Get build log safely (alternative to BUILD_LOG)
+    def logText = currentBuild.rawBuild.getLog(100).join('\n')
+    
     emailext(
         subject: "${currentBuild.currentResult}: ${env.JOB_NAME} [${env.BUILD_NUMBER}] - ${env.STAGE_NAME}",
         body: """<p><b>Project:</b> ${env.JOB_NAME}</p>
                <p><b>Status:</b> ${currentBuild.currentResult}</p>
-               ${currentBuild.currentResult == 'FAILURE' ? "<pre>${BUILD_LOG, maxLines=100}</pre>" : ""}""",
+               ${currentBuild.currentResult == 'FAILURE' ? """
+               <h3>Failure Log:</h3>
+               <pre>${logText}</pre>
+               """ : ""}""",
         to: recipient
     )
 }
